7bc0ca0b93422a6519ff47ef6c217abdf1a465b7
Laura Ramirez <lau.ramirez.encinas@gmail.com>
2020-04-29 20:02:53 +0200

    funcionColision

--- javascript/index.js
+++ javascript/index.js
@@ -2,26 +2,33 @@ var gameStarted = false
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+
 const juego = {
     //color="",
     bolas: [],
     //bolasRecolectadas=[],
-    //velocidad=0,
-    nivel:0,
-    contadorTick:0,  
-    velocidad: 1000,
-    
+    nivel: 0,
+    contadorTick: 0,
+    velocidad: 100,
+    vidas: 3,
+
+
 
     tick: function () {
-        this.nivel = parseInt(this.contadorTick/10)
-        let bola = new BolaDeHelado;
-        juego.bolas.forEach(el=> el.moverBola());
-        juego.bolas.push(bola);
+        this.nivel = parseInt(this.contadorTick / 100)
+        juego.bolas.forEach(el => el.moverBola());
+        if (this.contadorTick % 10== 0){
+            let bola = new BolaDeHelado;
+            juego.bolas.push(bola);
+        }
         this.render();
-        setTimeout(function(){
+        setTimeout(function () {
             juego.tick()
-        },this.velocidad - this.nivel *100 );
-        this.contadorTick++;   
+        }, this.velocidad - this.nivel * 10);
+        this.contadorTick++;
+        juego.checkCollision();
+
+        //window.requestAnimationFrame(tick);
     },
 
     render: function () {
@@ -32,14 +39,51 @@ const juego = {
         cono.dibujoCono();
     },
 
+    collision: function (cono, bola) {
+        let collisionX = (bola.x - 15) < cono.x  && (bola.x + 15) > cono.x;
+        let collisionY = bola.y > (cono.y - 100) && bola.y < cono.y;
 
+        if (collisionX && collisionY) {
+            return true;
+        } else {
+            return false
+        }
+    },
+
+    checkCollision: function () {
+        juego.bolas.forEach((el, index) => {
+            if (juego.collision(cono, el)) {
+                juego.pierdevida();
+                juego.bolas.splice(index,1);
+                console.log("COLISION");
+            }
+            // else if (el.y >= cono.y){
+            //     juego.bolas.splice(index,1);
+            // }
+
+        });
+    },
+
+    pierdevida: function () {
+        juego.vidas -= 1;
+        let mostrarVida = document.querySelector("#vidas")
+        mostrarVida.innerHTML = juego.vidas;
+    },
 }
+
+
+let mostrarVida = document.querySelector("#vidas")
+mostrarVida.innerHTML = juego.vidas;
+
+
+
 
 const cono = {
     //img : new Image
     x: 250,
     y: 650,
     speed: 20,
+
 
     dibujoCono() {
         ctx.lineWidth = 3;
@@ -50,7 +94,6 @@ const cono = {
         ctx.closePath();
         ctx.stroke();
     },
-
 
     //inicializar: function () {
     //    this.img = new Image();
@@ -69,8 +112,6 @@ const cono = {
         }
         juego.render();
     },
-
-
 }
 
 
@@ -79,7 +120,8 @@ class BolaDeHelado {
     constructor() {
         this.x = Math.floor(Math.random() * 500 - 50);
         this.y = 10;
-        this.vel=30;
+        this.vel = 3;
+
     }
     pintarBola() {
         ctx.beginPath();
@@ -89,7 +131,7 @@ class BolaDeHelado {
         ctx.closePath();
         ctx.stroke();
     }
-    moverBola(){
+    moverBola() {
         this.y += this.vel;
     }
 }
@@ -97,20 +139,25 @@ class BolaDeHelado {
 
 
 function startGame() {
-    if (gameStarted){
+    if (gameStarted) {
         return
     }
-    gameStarted=true;
+    gameStarted = true;
     juego.tick();
-    
 
 }
 
+function ocultar() {
+    document.querySelector('#start-button').style.display = 'none';
+    document.querySelector('#canvas').style.display = 'block';
+}
+
+
 window.onload = () => {
     document.getElementById('start-button').onclick = () => {
+        ocultar();
         startGame();
     };
-
 
     document.onkeydown = function (event) {
         cono.trataEventoTeclado(event);
@@ -118,7 +165,6 @@ window.onload = () => {
             cono.speed = -cono.speed
         }
     }
-
-
 };
+
 
